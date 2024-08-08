2f5e4e7b24ba84afe6f248e5e8ceb18a17ca4a4f
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-09 01:18:27 +0200

    Add Ownable func to Payment Token contract

--- contracts/ERC-20/PaymentToken.sol
+++ contracts/ERC-20/PaymentToken.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract PaymentToken is ERC20 {
-    constructor() ERC20("PaymentToken", "PT"){
+contract PaymentToken is ERC20, Ownable {
+    constructor() ERC20("PaymentToken", "PT") Ownable(msg.sender){
     }
 
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to,_amount);
     }
 }
