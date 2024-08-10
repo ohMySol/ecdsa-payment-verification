1c9522d9bcbe3483bb081771bfdfe9afffc2d4dd
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-10 23:56:41 +0200

    upd deploy scripts for BonusPayment and PaymentToken

--- deploy/00-deploy-paymentToken.js
+++ deploy/00-deploy-paymentToken.js
@@ -1,27 +1,27 @@
 const { network } = require("hardhat")
-const { devNetworks, networkConfig } = require("../utils/helpers/helper-hardhat")
+const { devNetworks, getNetworkConfig } = require("../utils/helpers/helper-hardhat")
 const { verify, tenderlyVerify } = require("../utils/contract-verification/verify")
 
 module.exports = async({deployments, getNamedAccounts}) => {
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
     const contractName = "PaymentToken"
+    const networkName = network.name
+    const config = await getNetworkConfig(networkName)
 
-    log(`\n============ Deploying PaymentToken contract to ${network.name} network ============\n`)
+    log(`\n============ Deploying PaymentToken contract to ${networkName} network ============\n`)
         
     const contract = await deploy(contractName, {
         from: deployer,
         log: true,
         args: [],
-        blockConfirmations: devNetworks.includes(network.name)
-        ? networkConfig.hardhat.blockConfirmations
-        : networkConfig.sepolia.blockConfirmations
+        blockConfirmations: config.blockConfirmations
     })
 
     log(`\n============ Contract deployed to: ${contract.address}  ============\n`)
 
-    if (!devNetworks.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        network.name == "sepolia"
+    if (!devNetworks.includes(networkName) && process.env.ETHERSCAN_API_KEY) {
+        networkName == "sepolia"
         ? await verify(contract.address, contract.args)
         : await tenderlyVerify(contractName, contract.address)
     } else {
--- deploy/01-deploy-bonusPayment.js
+++ deploy/01-deploy-bonusPayment.js
@@ -1,27 +1,29 @@
 const { network } = require("hardhat")
-const { devNetworks, networkConfig } = require("../utils/helpers/helper-hardhat")
+const { devNetworks, getNetworkConfig} = require("../utils/helpers/helper-hardhat")
 const { verify, tenderlyVerify } = require("../utils/contract-verification/verify")
+const { getDomainNameVersion} = require("../signing/data-to-sign")
 
 module.exports = async({deployments, getNamedAccounts}) => {
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
     const contractName = "BonusPayment"
+    const networkName = network.name
+    const config = await getNetworkConfig(networkName)
+    const {name, version} = await getDomainNameVersion()
 
-    log(`\n============ Deploying BonusPayment contract to ${network.name} network ============\n`)
+    log(`\n============ Deploying BonusPayment contract to ${networkName} network ============\n`)
         
     const contract = await deploy(contractName, {
         from: deployer,
         log: true,
-        args: [],
-        blockConfirmations: devNetworks.includes(network.name)
-        ? networkConfig.hardhat.blockConfirmations
-        : networkConfig.sepolia.blockConfirmations
+        args: [config.tokenAddress, name, version],
+        blockConfirmations: config.blockConfirmations
     })
 
     log(`\n============ Contract deployed to: ${contract.address}  ============\n`)
 
-    if (!devNetworks.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        network.name == "sepolia"
+    if (!devNetworks.includes(networkName) && process.env.ETHERSCAN_API_KEY) {
+        networkName == "sepolia"
         ? await verify(contract.address, contract.args)
         : await tenderlyVerify(contractName, contract.address)
     } else {
