2d94ac08257675092876dc5a05d71626aa41af99
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-16 22:28:11 +0200

    Add funcs for writing and reading v,r,s

--- utils/helpers/helper-hardhat.js
+++ utils/helpers/helper-hardhat.js
@@ -1,12 +1,25 @@
 const { ethers, JsonRpcProvider } = require("ethers");
 require("@chainlink/env-enc").config();
 require("dotenv").config()
-const fs = require('fs')
+const fsp = require('fs/promises');
 
-// For local testing
+/*
+1. This is the file with the helper configs and helper functions
+which you can use across your scripts in order to write less code.
+All this code is incapsulated in this module will help you dynamically
+deploy, write to contract, read from the contract, create providers,
+signers and contracts instances.
+*/
+
+
+
+// For local testing.
 const devNetworks = ["localhost", "hardhat"]
 
-// For creating contracts instances
+// Config object with all necessary data to create:
+// contract instance, create signer object, provider...
+// So that you will dynamically create all the above points
+// based on the network you are.
 const networkConfig = {
     hardhat: {
         blockConfirmations: 1,
@@ -54,6 +67,7 @@ const networkConfig = {
     }
 }
 
+// Returns the config object for specific 'networkName'.
 const getNetworkConfig = async(networkName) => {
     const config = networkConfig[networkName]
     if (!config) {
@@ -62,9 +76,10 @@ const getNetworkConfig = async(networkName) => {
     return config
 }
 
+// Returns a 'contractName' instance on the specific 'networkName'.
 getContractInstance = async(networkName, contractName) => {
     const config = await getNetworkConfig(networkName)
-    const data = JSON.parse(fs.readFileSync(`./deployments/${networkName}/${contractName}.json`, "utf8"))
+    const data = JSON.parse(await fsp.readFile(`./deployments/${networkName}/${contractName}.json`, "utf8"))
     const abi = data.abi
     const contractAddress = config.contracts[contractName]
 
@@ -75,10 +90,27 @@ getContractInstance = async(networkName, contractName) => {
     return contract 
 }
 
+// Write created signature parts(v,r,s) to the signatures.json file.
+writeSignature = async(v, r, s) => {
+    const signatureData = {
+            v: v,
+            r: r,
+            s: s
+    }
+    await fsp.writeFile('./signing/signatures.json', JSON.stringify(signatureData))
+}
+
+readSignature = async() => {
+   const {v, r, s} = JSON.parse(await fsp.readFile('./signing/signatures.json', 'utf8'))
+   return {v, r, s}
+}
+
 
 module.exports = {
     devNetworks,
     networkConfig,
     getNetworkConfig,
-    getContractInstance
+    getContractInstance,
+    writeSignature,
+    readSignature
 }
